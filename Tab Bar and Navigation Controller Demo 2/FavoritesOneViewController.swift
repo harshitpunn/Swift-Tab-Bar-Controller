0fb84b6ec9d4c5557fe8c25c99ae39cf1734ac69
Harshit Pun <harshitpunn@gmail.com>
2023-04-11 21:56:10 -0700

    Initial Commit

---
+++ Tab Bar and Navigation Controller Demo 2/FavoritesOneViewController.swift
@@ -0,0 +1,30 @@
+//
+//  FavoritesOneViewController.swift
+//  Tab Bar and Navigation Controller Demo 2
+//
+//  Created by Harshit Punn on 2023-04-03.
+//
+
+import UIKit
+
+class FavoritesOneViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        self.title = "Fone"
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Tab Bar and Navigation Controller Demo 2/RecentTwoViewController.swift
@@ -0,0 +1,30 @@
+//
+//  RecentTwoViewController.swift
+//  Tab Bar and Navigation Controller Demo 2
+//
+//  Created by Harshit Punn on 2023-04-03.
+//
+
+import UIKit
+
+class RecentTwoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        self.title = "Rtwo"
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
